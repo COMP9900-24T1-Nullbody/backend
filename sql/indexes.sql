1eb6eaf58af9f5b5a2c9f583050201d038fcaad6
feiii0506 <160138024+feiii0506@users.noreply.github.com>
2024-04-15 16:32:50 +1000

    indexes.sql

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,16 +1,14 @@
-
--- 【重要】这两个index用于限制google_id和microsoft_id的唯一性，勿删
--- 创建部分唯一索引以确保 google_id 在非空时唯一
+-- [Important] These two indexes are used to ensure the uniqueness of google_id and microsoft_id. Do not delete.
+-- Create a partial unique index to ensure google_id is unique when not empty
 DROP INDEX IF EXISTS idx_unique_google_id;
 CREATE UNIQUE INDEX idx_unique_google_id ON users (google_id) WHERE google_id <> '';
 
--- 创建部分唯一索引以确保 microsoft_id 在非空时唯一
+-- Create a partial unique index to ensure microsoft_id is unique when not empty
 DROP INDEX IF EXISTS idx_unique_microsoft_id;
 CREATE UNIQUE INDEX idx_unique_microsoft_id ON users (microsoft_id) WHERE microsoft_id <> '';
 
-
--- 【次重要】这里的index用于加速查询与聚合，删除可能导致性能下降
--- 为连接使用的列创建索引
+-- [Less Important] These indexes are used for query acceleration and aggregation. Removing them may lead to performance degradation.
+-- Create indexes for columns used in joins
 DROP INDEX IF EXISTS idx_scores_company_id;
 CREATE INDEX idx_scores_company_id ON scores (company_id);
 
@@ -32,7 +30,7 @@ CREATE INDEX idx_indicator_weights_indicator_id ON indicator_weights (indicator_
 DROP INDEX IF EXISTS idx_frameworks_id;
 CREATE INDEX idx_frameworks_id ON frameworks (id);
 
--- 为查询条件、分组和排序使用的列创建索引
+-- Create indexes for columns used in query conditions, grouping, and sorting
 DROP INDEX IF EXISTS idx_companies_name;
 CREATE INDEX idx_companies_name ON companies (name);
 
@@ -51,6 +49,6 @@ CREATE INDEX idx_indicator_weights_framework_id ON indicator_weights (framework_
 DROP INDEX IF EXISTS idx_metric_weights_metric_id;
 CREATE INDEX idx_metric_weights_indicator_id ON metric_weights (indicator_id);
 
--- 为 AVG(scores.score) 的计算加速
+-- Accelerate the calculation of AVG(scores.score)
 DROP INDEX IF EXISTS idx_scores_score;
 CREATE INDEX idx_scores_score ON scores (score);
